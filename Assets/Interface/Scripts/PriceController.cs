7fe2878537ed820a1896aeffe52af63a9dfffe67
ArsPetr <arseniyp1@yandex.ru>
2024-07-19 22:41:30 +0300

    Added price changer

---
+++ Assets/Interface/Scripts/PriceController.cs
@@ -0,0 +1,44 @@
+using TMPro;
+using UnityEngine;
+
+public class PriceController : MonoBehaviour
+{
+    private int _price = 0;
+
+    [SerializeField]
+    private TextMeshProUGUI _textMeshPro;
+    public delegate void OnPriceChanged(int price);
+    public OnPriceChanged priceChanged;
+
+    public void Start()
+    {
+        UpdatePriceText();
+    }
+
+    public void ConfirmPrice()
+    {
+
+    }
+
+    public void changePrice(int dec)
+    {
+
+        if (dec >= 0)
+        {
+            _price = (_price * 10) + dec;
+        }
+        else
+        {
+            _price /= 10;
+        }
+        if (_price > 10000)
+            _price = 9999;
+        priceChanged?.Invoke(_price);
+        UpdatePriceText();
+    }
+
+    private void UpdatePriceText()
+    {
+        _textMeshPro.text = _price.ToString();
+    }
+}
